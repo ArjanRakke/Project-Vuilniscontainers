240e7ea394a28b8ced6947319b53b0ca72ed7643
Jesse <jessepols@gmail.com>
2019-01-22 23:44:38 +0100

    tkinter anders ingedeeld en grafiek toegevoegd soortvan

--- Frames.py
+++ Frames.py
@@ -1,21 +1,106 @@
-from tkinter import *
-# from PIL import Image, ImageTk
+import tkinter as gui
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+import numpy as np
 
 
-class EmptyFrame(Frame):
+# Empty frame for testing
+class EmptyFrame(gui.Frame):
     def __init__(self, master, controller):
-        Frame.__init__(self, master)
-        self.button = Button(self, text='EmptyFrame', command=lambda: controller.show_frame(ContainerFrame))
+        gui.Frame.__init__(self, master)
+        self.label = gui.Label(self, text='Empty').grid()
+
+
+# CONTAINER FRAMES
+# How full & basic data
+# Emptying Data
+
+class ContainerEmptyFrame(gui.Frame):
+    def __init__(self, master, controller):
+        gui.Frame.__init__(self, master)
+        self.label = gui.Label(self, text='Empty Container').grid()
+
+        # self.trash = {"day": ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"], "trash_kilo": [5, 7, 4, 8, 6, 12, 9]}
+        fig = Figure(figsize=(5, 4), dpi=100)
+        t = np.arange(0, 3, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+        canvas = FigureCanvasTkAgg(fig, master=self)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().grid()
+        # canvas.get_tk_widget().pack(side=gui.TOP, fill=gui.BOTH, expand=1)
+
+        # df = pd.DataFrame(self.trash)
+        # df.set_index("dag", inplace=True)
+
+        # plt.bar(self.trash["dag"], self.trash["kilos_afval"])
+
+        # plt.xlabel("dag")
+        # plt.ylabel("kilo afval")
+        # plt.title("placeholder")
+        # plt.show()
+        # print(df)
+
+
+class ContainerDataFrame(gui.Frame):
+    def __init__(self, master, controller):
+        gui.Frame.__init__(self, master)
+        self.label = gui.Label(self, text='Data').grid()
+        self.label_location = gui.Label(self, text='Location').grid()
+        self.label_percentage = gui.Label(self, text='Container at 0%')
+
+
+# OTHER FRAMES
+# Settings
+# Graphs that concern all containers
+
+class SettingsFrame(gui.Frame):
+    def __init__(self, master, controller):
+        gui.Frame.__init__(self, master)
+        self.label = gui.Label(self, text='Settings').grid()
+
+
+class GraphFrame(gui.Frame):
+    def __init__(self, master, controller):
+        gui.Frame.__init__(self, master)
+        self.label = gui.Label(self, text='Graph').grid()
+
+
+""""
+# Every container gets a [Container Frame], displaying all important data
+class ContainerFrame(gui.Frame):
+    def __init__(self, master, controller):
+        gui.Frame.__init__(self, master)
+        self.button = gui.Button(self, text='ContainerFrame', command=lambda: controller.show_frame(EmptyFrame))
         self.button.grid(row=0, column=0)
 
+        # Container % filled up
+        # Container name
+        # Container location
+        # Last time emptied
 
-class ContainerFrame(Frame):
+
+class MenuFrame(gui.Frame):
     def __init__(self, master, controller):
-        Frame.__init__(self, master)
-        self.button = Button(self, text='ContainerFrame', command=lambda: controller.show_frame(EmptyFrame))
-        self.button.grid(row=0, column=0)
+        gui.Frame.__init__(self, master)
 
-        # img = ImageTk.PhotoImage(Image.open('Project/container_empty.png'))
-        # self.label = Label(self, image=img)
-        # self.label.image = img
-        # self.label.grid()
+        self.button_exit = gui.Button(self, text='Exit', command=lambda: self.exit_this())
+        self.button_exit.grid()
+
+    def exit_this(self):
+        sys.exit(0)
+
+        # Graph
+        # Basic Info
+        # Empty
+        # Exit
+        # Next
+
+"""
--- Main.py
+++ Main.py
@@ -1,23 +1,59 @@
-from tkinter import *
-from Frames import EmptyFrame, ContainerFrame
+import tkinter as gui
+import tkinter.messagebox
+from Frames import EmptyFrame, ContainerDataFrame, ContainerEmptyFrame, SettingsFrame, GraphFrame
 
 
-class MainApplication(Tk):
+class MainApplication(gui.Tk):
     def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        container = Frame(self)
-        container.grid()
+        gui.Tk.__init__(self, *args, **kwargs)
 
-        self.grid_rowconfigure(0, weight=1)
+        # Toolbar
+        toolbar = gui.Frame(self, bd=1, relief='raised')
+        toolbar.pack(side='top', fill='x')
+
+        # Container contains everything
+        container = gui.Frame(self)
+        container.pack(fill='x')
+
+        #self.geometry('768x432')
         self.frames = {}
 
-        for F in (EmptyFrame, ContainerFrame):
+        # Initializes frames with [container] as master and [self] as controller
+        for F in (EmptyFrame, ContainerDataFrame, ContainerEmptyFrame, SettingsFrame, GraphFrame):
             self.frames[F] = F(container, self)
-            self.frames[F].grid(row=0, column=0, sticky='news')
-        self.show_frame(ContainerFrame)
+            self.frames[F].grid(row=0, sticky='news')
+        self.show_frame(ContainerEmptyFrame)
 
+        button_container_empty = gui.Button(toolbar, text='Container Emptying', command=lambda: self.show_frame(ContainerEmptyFrame))
+        button_container_data = gui.Button(toolbar, text='Container Data', command=lambda: self.show_frame(ContainerDataFrame))
+        button_settings = gui.Button(toolbar, text='Settings', command=lambda: self.show_frame(SettingsFrame))
+        button_graph = gui.Button(toolbar, text='Graph', command=lambda: self.show_frame(GraphFrame))
+        button_empty = gui.Button(toolbar, text='Empty', command=lambda: self.show_frame(EmptyFrame))
+
+        button_container_empty.pack(side='left', padx=2, pady=2)
+        button_container_data.pack(side='left', padx=2, pady=2)
+        button_settings.pack(side='left', padx=2, pady=2)
+        button_graph.pack(side='left', padx=2, pady=2)
+        button_empty.pack(side='left', padx=2, pady=2)
+
+    # Show specific frame
     def show_frame(self, cont):
         self.frames[cont].tkraise()
+
+    # Pop up messages
+    @staticmethod
+    def message_box(type_message, message):
+        if type_message == 'info':
+            gui.messagebox.showinfo('Info', message)
+        if type_message == 'error':
+            gui.messagebox.showerror('Error', message)
+        if type_message == 'warning':
+            gui.messagebox.showwarning('Warning', message)
+
+    # Gets data from database
+    @staticmethod
+    def database():
+        return None
 
 
 app = MainApplication()
