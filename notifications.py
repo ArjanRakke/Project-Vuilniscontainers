ad477215b2fd6dcd65d4fbdd6a770498543dbea0
ArjanRakke <arakke@ziggo.nl>
2019-01-23 16:18:33 +0100

    notificaties via Win10

--- notifications.py
+++ notifications.py
@@ -1,6 +1,8 @@
 from win10toast import ToastNotifier
 
+# checkt hoeveel capaciteit in gebruik is
 def check_capaciteit(container):
+    # als de container 80 procent capaciteit of hoger heeft stuur een notificatie.
     if container >= 80:
         toaster = ToastNotifier()
         toaster.show_toast("Container bijna vol", "Container moet geleegd worden")
